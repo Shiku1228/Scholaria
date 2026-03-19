e1ea1b774a3fcbeba47776de2a2f9e61623a4e5c
Shiku1228 <latanggar2@gmail.com>
2026-03-19 11:44:38 +0800

    Added the login page for the android interface of Scholaria. Implemented also the dynamic designing for the theme.

---
+++ app/src/main/java/com/example/scholaria/AuthActivity.java
@@ -0,0 +1,26 @@
+package com.example.scholaria;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_auth);
+        
+        View mainView = findViewById(android.R.id.content);
+        ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
